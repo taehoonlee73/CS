d1693a66834bbb304111fddf404a69262a662092
taehoon lee <thlee@dacpole.com>
2019-06-26 13:56:22 +0900

    초기 커밋.

---
+++ SchedulerCustomAppointmentForm/CustomAppointmentForm.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+#region #usings_form
+using DevExpress.Xpf.Scheduler;
+using DevExpress.Xpf.Scheduler.UI;
+using DevExpress.XtraScheduler;
+#endregion #usings_form
+
+namespace WpfApplication1 {
+    /// <summary>
+    /// Interaction logic for CustomAppointmentForm.xaml
+    /// </summary>
+#region #customform
+    public partial class CustomAppointmentForm : UserControl {
+
+        public CustomAppointmentFormController Controller { get; private set; }
+        public SchedulerControl Control { get; private set; }
+        public Appointment Appointment { get; private set; }
+        public string TimeEditMask { get { return CultureInfo.CurrentCulture.DateTimeFormat.LongTimePattern; } }
+
+
+        public CustomAppointmentForm(SchedulerControl control, Appointment apt) {
+            InitializeComponent();
+            if(control == null || apt == null)
+                throw new ArgumentNullException("control");
+            if(control == null || apt == null)
+                throw new ArgumentNullException("apt");
+
+            this.Control = control;
+            this.Controller = new CustomAppointmentFormController(control, apt);
+            this.Appointment = apt;
+        }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e) {
+            if(Controller.IsNewAppointment)
+                SchedulerFormBehavior.SetTitle(this, "New appointment");
+            else
+                SchedulerFormBehavior.SetTitle(this, "Edit - [" + Appointment.Subject + "]");
+        }
+
+        private void Ok_button_Click(object sender, System.Windows.RoutedEventArgs e) {
+            // Save all changes made to the appointment.            
+            Controller.Control.Storage.BeginUpdate();
+            Controller.ApplyChanges();
+            Controller.Control.Storage.EndUpdate();
+            SchedulerFormBehavior.Close(this, true);
+        }
+
+        private void Cancel_button_Click(object sender, System.Windows.RoutedEventArgs e) {
+            SchedulerFormBehavior.Close(this, false);
+        }
+    }
+#endregion #customform
+
+#region #controller
+    public class CustomAppointmentFormController : AppointmentFormController {
+        public CustomAppointmentFormController(SchedulerControl control, Appointment apt)
+            : base(control, apt) {
+        }
+
+        public string Contact {
+            get { return GetContactValue(EditedAppointmentCopy); }
+            set { EditedAppointmentCopy.CustomFields["Contact"] = value; }
+        }
+
+        string SourceContact {
+            get { return GetContactValue(SourceAppointment); }
+            set { SourceAppointment.CustomFields["Contact"] = value; }
+        }
+
+        public override bool IsAppointmentChanged() {
+            if(base.IsAppointmentChanged())
+                return true;
+            return SourceContact != Contact;
+        }
+
+        protected string GetContactValue(Appointment apt) {
+            return Convert.ToString(apt.CustomFields["Contact"]);
+        }
+    }
+#endregion #controller
+}
---
+++ SchedulerCustomAppointmentForm/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Windows;
+#region #usings
+using System.Data.OleDb;
+using DevExpress.XtraScheduler;
+#endregion #usings
+
+namespace WpfApplication1 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        CarsDBDataSet dataSet;
+        CarsDBDataSetTableAdapters.CarSchedulingTableAdapter adapter;
+
+        public MainWindow() {
+            InitializeComponent();
+            this.dataSet = new CarsDBDataSet();
+
+            // Bind the scheduler storage to appointment data.
+            this.CarSchedulingControl.Storage.AppointmentStorage.DataSource = dataSet.CarScheduling;
+
+            // Load data into the 'CarsDBDataSet.CarScheduling' table. 
+            this.adapter = new CarsDBDataSetTableAdapters.CarSchedulingTableAdapter();
+            this.adapter.Fill(dataSet.CarScheduling);
+
+            this.CarSchedulingControl.Storage.AppointmentsInserted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.CarSchedulingControl.Storage.AppointmentsChanged +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.CarSchedulingControl.Storage.AppointmentsDeleted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+
+            this.adapter.Adapter.RowUpdated +=
+                new System.Data.OleDb.OleDbRowUpdatedEventHandler(adapter_RowUpdated);
+
+            // Bind the scheduler storage to resource data.  
+            this.CarSchedulingControl.Storage.ResourceStorage.DataSource = dataSet.Cars;
+
+            // Load data into the 'CarsDBDataSet.Cars' table.  
+            CarsDBDataSetTableAdapters.CarsTableAdapter carsAdapter = new CarsDBDataSetTableAdapters.CarsTableAdapter();
+            carsAdapter.Fill(dataSet.Cars);
+        }
+        void Storage_AppointmentsModified(object sender, PersistentObjectsEventArgs e)
+        {
+            this.adapter.Adapter.Update(this.dataSet);
+            this.dataSet.AcceptChanges();
+        }
+
+        private void adapter_RowUpdated(object sender, System.Data.OleDb.OleDbRowUpdatedEventArgs e)
+        {
+            if (e.Status == UpdateStatus.Continue && e.StatementType == StatementType.Insert)
+            {
+                int id = 0;
+                using (OleDbCommand cmd = new OleDbCommand("SELECT @@IDENTITY", adapter.Connection))
+                {
+                    id = (int)cmd.ExecuteScalar();
+                }
+                e.Row["ID"] = id;
+            }
+        }
+
+        #region #editappointmentformshowing
+        private void scheduler_EditAppointmentFormShowing(object sender, DevExpress.Xpf.Scheduler.EditAppointmentFormEventArgs e) {
+            e.Form = new CustomAppointmentForm(CarSchedulingControl, e.Appointment);
+        }
+        #endregion #editappointmentformshowing
+    }
+}
